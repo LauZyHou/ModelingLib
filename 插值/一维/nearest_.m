43cceffaca0cba3c76af5466861ce8cdbcd24285
刘知昊 <java233@foxmail.com>
2019-09-07 10:49:49 +0800

    MATLAB插值

---
+++ 插值/一维/nearest_.m
@@ -0,0 +1,13 @@
+clear;clc;
+% -5~5取11个基点
+x0 = linspace(-5,5,11);
+y0 = 1 ./ (1+x0.^2);
+
+% 最近邻插值
+x = linspace(-5,5,100);
+y = interp1(x0,y0,x,'nearest');
+
+% 绘制和真实函数的比较图
+x1 = linspace(-5,5,100);
+y1 = 1 ./ (1+x1.^2);
+plot(x1,y1,'k',x0,y0,'o',x,y,'r');
